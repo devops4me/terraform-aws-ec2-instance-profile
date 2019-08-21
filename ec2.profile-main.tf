ea6b47896ba4ca64b12a5577101e6645541d4d95
Apollo Akora <apolloakora@gmail.com>
2019-08-21 10:17:38 +0100

    refactored the name of policy statements to be more specific

--- ec2.profile-main.tf
+++ ec2.profile-main.tf
@@ -3,7 +3,7 @@
  | -- This ec2 role is effectively a bucket for the policies and is specifying
  | -- that an ec2 instance (as opposed to a RDS database or EKS cluster) is the
  | -- vessel that will gain access as defined by the policies passed in through
- | -- the in_policy_stmts variable.
+ | -- the in_ec2_policy_stmts variable.
  | --
 */
 resource aws_iam_role ec2_instance_role {
@@ -26,7 +26,7 @@ resource aws_iam_role ec2_instance_role {
 resource aws_iam_role_policy ec2_instance_policy {
     name   = "ec2-policy-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
     role   = aws_iam_role.ec2_instance_role.id
-    policy = var.in_policy_stmts
+    policy = var.in_ec2_policy_stmts
 }
 
 
